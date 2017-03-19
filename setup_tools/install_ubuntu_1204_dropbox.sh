7d3d8e5bf7ed296630fee8de63cb58a6ee09b63f
trodevel <trodevel@gmail.com>
2017-03-20 00:31:39 +0100

    moved system setup tools into setup_tools folder

---
+++ setup_tools/install_ubuntu_1204_dropbox.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install Dropbox in Ubuntu 12.04
+#
+# Copyright (C) 2015 Sergey Kolevatov
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+# source https://www.liberiangeek.net/2012/04/install-dropbox-in-ubuntu-12-04-precise-pangolin/
+
+sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
+sudo add-apt-repository "deb http://linux.dropbox.com/ubuntu $(lsb_release -sc) main"
+sudo apt-get update && sudo apt-get install dropbox
