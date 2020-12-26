9866304f53d87c7c64b5773a57224b6d2229e990
trodevel <trodevel@gmail.com>
2020-12-26 19:50:14 +0100

    added optional prefix to filename

--- ytdl.sh
+++ ytdl.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 URL=$1
+PREF=$2
 
-[[ -z "$URL" ]] && echo "USAGE: $0 <URL>" && exit
+[[ -z "$URL" ]] && echo "USAGE: $0 <URL> [<PREF>]" && exit
 
-youtube-dl -x --embed-thumbnail --audio-format mp3 --add-metadata --audio-quality 0 $URL
+if [[ -z "$PREF" ]]
+then
+    youtube-dl -x --embed-thumbnail --audio-format mp3 --add-metadata --audio-quality 0 $URL
+else
+    youtube-dl -x --embed-thumbnail --audio-format mp3 --add-metadata --audio-quality 0 -o "$PREF - %(title)s.%(ext)s" $URL
+fi
