6d5caf8d4a09b83f41df772965da38f52bc486b3
trodevel <trodevel@gmail.com>
2021-01-09 15:48:47 +0100

    added statistics

--- dl_yt_channel_list_by_name.sh
+++ dl_yt_channel_list_by_name.sh
@@ -9,4 +9,10 @@ DATUM=$(date -u +%Y%m%d)
 
 [[ -n $DATE ]] && PAR="--dateafter $DATE"
 
-youtube-dl -j --flat-playlist $PAR "https://www.youtube.com/c/$NAME/videos"  | jq -r '.id' | sed 's_^_https://youtu.be/_' > channel_${NAME}_${DATUM}.txt
+LIST_NAME=channel_${NAME}_${DATUM}.txt
+
+youtube-dl -j --flat-playlist $PAR "https://www.youtube.com/c/$NAME/videos"  | jq -r '.id' | sed 's_^_https://youtu.be/_' > $LIST_NAME
+
+echo -n "number of videos : "
+
+cat $LIST_NAME | wc -l
