0032ea19f8929ea82d73c6278c02ce9d359b5fc6
trodevel <trodevel@gmail.com>
2021-01-13 18:33:22 +0100

    write the filelist in reverse direction. Reason: to keep chronological order

--- dl_yt_channel_list_by_name.sh
+++ dl_yt_channel_list_by_name.sh
@@ -11,7 +11,7 @@ DATUM=$(date -u +%Y%m%d)
 
 LIST_NAME=channel_${NAME}_${DATUM}.txt
 
-youtube-dl -j --flat-playlist $PAR "https://www.youtube.com/c/$NAME/videos"  | jq -r '.id' | sed 's_^_https://youtu.be/_' > $LIST_NAME
+youtube-dl -j --flat-playlist $PAR "https://www.youtube.com/c/$NAME/videos"  | jq -r '.id' | sed 's_^_https://youtu.be/_' | tac > $LIST_NAME
 
 echo -n "number of videos : "
 
