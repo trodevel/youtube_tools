5adcb236e4e33ed12c992df9828b90f2ea48b465
trodevel <trodevel@gmail.com>
2021-02-02 22:21:16 +0100

    1. removed PAR 2. added parameter LIST_NAME

--- dl_yt_channel_list_by_id.sh
+++ dl_yt_channel_list_by_id.sh
@@ -2,18 +2,16 @@
 
 ID=$1
 NAME=$2
-DATE=$3
+LIST_NAME=$3
 
 [[ -z $ID ]] && echo "ERROR: ID is not given" && exit
 [[ -z $NAME ]] && echo "ERROR: NAME is not given" && exit
 
 DATUM=$(date -u +%Y%m%d)
 
-[[ -n $DATE ]] && PAR="--dateafter $DATE"
+[[ -z $LIST_NAME ]] && LIST_NAME=channel_${NAME}_${DATUM}.txt
 
-LIST_NAME=channel_${NAME}_${DATUM}.txt
-
-youtube-dl -j --flat-playlist $PAR "https://www.youtube.com/channel/$ID/videos"  | jq -r '.id' | sed 's_^_https://youtu.be/_' | tac > $LIST_NAME
+youtube-dl -j --flat-playlist "https://www.youtube.com/channel/$ID/videos"  | jq -r '.id' | sed 's_^_https://youtu.be/_' | tac > $LIST_NAME
 
 echo -n "number of videos : "
 
--- dl_yt_channel_list_by_name.sh
+++ dl_yt_channel_list_by_name.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 NAME=$1
-DATE=$2
+LIST_NAME=$2
 
 [[ -z $NAME ]] && echo "ERROR: NAME is not given" && exit
 
 DATUM=$(date -u +%Y%m%d)
 
-[[ -n $DATE ]] && PAR="--dateafter $DATE"
+[[ -z $LIST_NAME ]] && LIST_NAME=channel_${NAME}_${DATUM}.txt
 
-LIST_NAME=channel_${NAME}_${DATUM}.txt
-
-youtube-dl -j --flat-playlist $PAR "https://www.youtube.com/c/$NAME/videos"  | jq -r '.id' | sed 's_^_https://youtu.be/_' | tac > $LIST_NAME
+youtube-dl -j --flat-playlist "https://www.youtube.com/c/$NAME/videos"  | jq -r '.id' | sed 's_^_https://youtu.be/_' | tac > $LIST_NAME
 
 echo -n "number of videos : "
 
