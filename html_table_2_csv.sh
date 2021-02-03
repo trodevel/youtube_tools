1f4f5f69edf4b08cb1a67904943c783f6f0a8ad6
trodevel <trodevel@gmail.com>
2021-02-03 03:39:35 +0100

    removed irrelevant files

--- html_table_2_csv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# http://stackoverflow.com/questions/1403087/how-can-i-convert-an-html-table-to-csv
-
-grep -i -e '</\?TABLE\|</\?TD\|</\?TR\|</\?TH' | sed 's/^[\ \t]*//g' | tr -d '\n\r' | sed 's/<\/TR[^>]*>/\n/Ig'  | sed 's/<\/\?\(TABLE\|TR\)[^>]*>//Ig' | sed 's/^<T[DH][^>]*>\|<\/\?T[DH][^>]*>$//Ig' | sed 's/<\/T[DH][^>]*><T[DH][^>]*>/;/Ig'
